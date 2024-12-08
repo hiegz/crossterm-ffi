11226abd67ac7029604330dade988188bfea4ea5
Volodymyr But <git@hiegz.com>
2024-12-08 23:29:56 +0100

    feat: add scrolling commands

--- crossterm-rs/src/terminal.rs
+++ crossterm-rs/src/terminal.rs
@@ -24,3 +24,29 @@ pub unsafe extern "C" fn crossterm_leave_alternate_screen(
         0
     }
 }
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_scroll_up(
+    stream: *mut crossterm_stream,
+    nlines: u16,
+) -> libc::c_int {
+    let ret = crossterm::queue!((&mut *stream), crossterm::terminal::ScrollUp(nlines));
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_scroll_down(
+    stream: *mut crossterm_stream,
+    nlines: u16,
+) -> libc::c_int {
+    let ret = crossterm::queue!((&mut *stream), crossterm::terminal::ScrollDown(nlines));
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
--- include/crossterm_ffi/terminal.h
+++ include/crossterm_ffi/terminal.h
@@ -1,9 +1,13 @@
 #ifndef CROSSTERM_FFI_TERMINAL_H
 #define CROSSTERM_FFI_TERMINAL_H
 
+#include <stdint.h>
+
 struct crossterm_stream;
 
 int crossterm_enter_alternate_screen(struct crossterm_stream *stream);
 int crossterm_leave_alternate_screen(struct crossterm_stream *stream);
+int crossterm_scroll_up(struct crossterm_stream *stream, uint16_t nlines);
+int crossterm_scroll_down(struct crossterm_stream *stream, uint16_t nlines);
 
 #endif
