4553ca34ac3742596e46276baa67712b1562bd0e
Volodymyr But <git@hiegz.com>
2024-12-09 13:04:58 +0100

    feat: add commands to control cursor position

--- crossterm-rs/src/cursor.rs
+++ crossterm-rs/src/cursor.rs
@@ -20,3 +20,135 @@ pub unsafe extern "C" fn crossterm_hide_cursor(stream: *mut crossterm_stream) ->
         0
     }
 }
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_save_cursor_position(
+    stream: *mut crossterm_stream,
+) -> libc::c_int {
+    let ret = crossterm::queue!((&mut *stream), crossterm::cursor::SavePosition);
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_restore_cursor_position(
+    stream: *mut crossterm_stream,
+) -> libc::c_int {
+    let ret = crossterm::queue!((&mut *stream), crossterm::cursor::RestorePosition);
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_move_cursor_up(
+    stream: *mut crossterm_stream,
+    nrows: u16,
+) -> libc::c_int {
+    let ret = crossterm::queue!((&mut *stream), crossterm::cursor::MoveUp(nrows));
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_move_cursor_down(
+    stream: *mut crossterm_stream,
+    nrows: u16,
+) -> libc::c_int {
+    let ret = crossterm::queue!((&mut *stream), crossterm::cursor::MoveDown(nrows));
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_move_cursor_left(
+    stream: *mut crossterm_stream,
+    ncols: u16,
+) -> libc::c_int {
+    let ret = crossterm::queue!((&mut *stream), crossterm::cursor::MoveLeft(ncols));
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_move_cursor_right(
+    stream: *mut crossterm_stream,
+    ncols: u16,
+) -> libc::c_int {
+    let ret = crossterm::queue!((&mut *stream), crossterm::cursor::MoveRight(ncols));
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_move_cursor_to(
+    stream: *mut crossterm_stream,
+    row: u16,
+    col: u16,
+) -> libc::c_int {
+    let ret = crossterm::queue!((&mut *stream), crossterm::cursor::MoveTo(row, col));
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_move_cursor_to_row(
+    stream: *mut crossterm_stream,
+    row: u16,
+) -> libc::c_int {
+    let ret = crossterm::queue!((&mut *stream), crossterm::cursor::MoveToRow(row));
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_move_cursor_to_next_line(
+    stream: *mut crossterm_stream,
+    nlines: u16,
+) -> libc::c_int {
+    let ret = crossterm::queue!((&mut *stream), crossterm::cursor::MoveToNextLine(nlines));
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn crossterm_move_cursor_to_previous_line(
+    stream: *mut crossterm_stream,
+    nlines: u16,
+) -> libc::c_int {
+    let ret = crossterm::queue!(
+        (&mut *stream),
+        crossterm::cursor::MoveToPreviousLine(nlines)
+    );
+    if let Err(err) = ret {
+        -(crossterm_error::from(err) as i32)
+    } else {
+        0
+    }
+}
